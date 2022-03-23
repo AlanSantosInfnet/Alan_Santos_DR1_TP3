0f3059e55930823403f1e30d4bebe71e8b8444ef
Alan Cristian dos Santo <alan.santos@al.infnet.edu.br>
2022-03-23 16:06:49 -0300

    Questão 03

---
+++ Questão03.py
@@ -0,0 +1,17 @@
+
+nome_vencedor = ''
+nota_vencedor = 0.0
+
+for contador in range(1,6):
+    nome = input(f'Informe o nome do {contador}º participante: ')
+    nota = float(input(f'Informe a nota do {contador}° participante: '))
+
+    if nota >= 0 and nota <= 10:
+        if nota > nota_vencedor:
+            nome_vencedor = nome
+            nota_vencedor = nota
+    else:
+        print('Nota Invalida!!!')
+
+print(f'O(a) vencedor(a) foi {nome_vencedor} com nota {nota_vencedor}!')
+
